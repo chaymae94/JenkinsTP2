891e74fe5dfaf62ab794e131d5918e02050ce4d9
BENDAOUD <chaymae@chaymae.localdomain>
2021-07-13 02:14:14 -0700

    Commit TP2

---
+++ TP2.py
@@ -0,0 +1,9 @@
+print("Combien du temps avez-vous vécu en nombre de jours, de minutes et de secondes.")
+nom = input("nom:")
+print("merci d'indiquer votre age")
+age = int(input("age :"))
+jours = age * 365
+minutes = age * 525948
+secondes = age * 31556926
+print(nom, "a vécu", jours, "jours", minutes, "minutes et", secondes, "secondes ! Wow !")
+
