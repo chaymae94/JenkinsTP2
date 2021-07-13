b5759ad5306fdc9dcd961420a15fd44abfc2e7e5
BENDAOUD <chaymae@chaymae.localdomain>
2021-07-13 04:38:21 -0700

    2eme modification TP2

--- TP2.py
+++ TP2.py
@@ -5,5 +5,5 @@ age = int(input("age :"))
 jours = age * 365
 minutes = age * 525948
 secondes = age * 31556926
-print(nom, "a vécu", jours, "jours", minutes, "minutes et", secondes, "secondes ! Wow !")
+print(nom, "a vécu", jours, "date du jours", minutes, "minutes et", secondes, "secondes ! Wow !")
 
